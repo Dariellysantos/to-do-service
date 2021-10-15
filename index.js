37306e2993128bd2837046a8fa185e54c4126eb3
Darielly Santos <daryh_cristina@hotmail.com>
2021-10-15 18:29:56 -0300

    fix: corrigi a url do endpoint para buscar uma task

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const listController = require("./controller/listController");
 app.use(express.json());
 
 app.get("/tasks", taskController.getTasks);
-app.get("/tasks/:id", taskController.getTask);
+app.get("/task/:id", taskController.getTask);
 app.post("/task", taskController.createTask);
 app.put("/task/:id", taskController.changeTask);
 app.delete("/task/:id", taskController.deleteTask);
